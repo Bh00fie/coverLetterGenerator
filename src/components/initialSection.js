b4baada433006e62ba9c7d71c2c0fef3322503fe
Abhi <Bh00fie@gmail.com>
2023-07-30 13:51:57 +0100

    create skeletron without styling for the initial section

--- src/components/initialSection.js
+++ src/components/initialSection.js
@@ -3,10 +3,15 @@ import "./initialSection.css"
 
 function InitialSection() {
     return (
-        <div>
-            <h1>
-                Initial Section
-            </h1>
+        <div id="initialSection">
+            <div id="userAccount">
+                <h3> Sign Up </h3>
+                <h3> Login </h3>
+            </div>
+            <div id="webpageTitleSection">
+                <h2 id="webpageTitle"> Cover Letter Generator using AI </h2>
+                <h3 id="webpageDescription"> Create your next cover letter in two minutes!</h3>
+            </div>
         </div>
     );
 }
