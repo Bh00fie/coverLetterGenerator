c4a38aeb8ac2ecb900840217f2b5db7580774fe3
Abhi <Bh00fie@gmail.com>
2023-07-30 16:30:57 +0100

    create a section to allow use to write down his CV or attacch the file, chatGPT has been used to solve some JS errors as I wasn't finding anythign online

--- src/components/UserInput/cvSection.js
+++ src/components/UserInput/cvSection.js
@@ -1,14 +1,47 @@
-import React from "react"
-import "./cvSection.css"
+import React, { useState } from "react";
+import "./cvSection.css";
 
 function CV() {
-    return (
-        <div>
-            <h1>
-                A
-            </h1>
+  const [fileName, setFileName] = useState("");
+
+  const handleFileChange = (e) => {
+    const input = e.target;
+
+    let newFileName = "";
+    if (input.files && input.files.length > 0) {
+      newFileName = input.files[0].name;
+    }
+
+    setFileName(newFileName); // Set the state with the new file name
+  };
+
+  return (
+    <div id="CVInput">
+      <div id="CVInfoInput">
+        <textarea className="CVSection" id="CVtext" name="CV" placeholder="CV" required />
+        <div className="CVSection" id="CVAttach">
+          <input
+            type="file"
+            name="file"
+            id="file"
+            className="inputfile"
+            data-multiple-caption="{count} files selected"
+            multiple
+            onChange={handleFileChange}
+          />
+          <label htmlFor="file" id="fileUpload">
+            {fileName ? (
+              <span>{fileName}</span>
+            ) : (
+              <>
+                Choose a file<span></span>
+              </>
+            )}
+          </label>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default CV;
--- src/components/UserInput/personalInfo.js
+++ src/components/UserInput/personalInfo.js
@@ -4,11 +4,11 @@ import "./personalInfo.css"
 function UserInfo() {
     return (
         <div id="personalInput">
-        <div id='personalInfoInput'>
-            <input className='input' id='userName' placeholder='Full Name' type='text' name='user_name' required/>
-            <input className='input' id='userPosition' placeholder='Position' type='text' name='user_position' required/>
-            <input className='input' id='userCompany' placeholder='Company Name' type='text' name='user_company' required/>
-        </div>
+            <div id='personalInfoInput'>
+                <input className='input' id='userName' placeholder='Full Name' type='text' name='user_name' required/>
+                <input className='input' id='userPosition' placeholder='Position' type='text' name='user_position' required/>
+                <input className='input' id='userCompany' placeholder='Company Name' type='text' name='user_company' required/>
+            </div>
         </div>
     );
 }
