75d2abf2c70638e4fb4870d9aad7b33528782463
Abhi <Bh00fie@gmail.com>
2023-07-31 20:11:38 +0100

    added footer and basic styling, this needs to be improved

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,27 @@
 import React from "react"
 import "./footer.css"
+import icon from './images/icon.png'
 
 function Footer() {
     return (
-        <div>
-            <h1>
-                Footer
-            </h1>
-        </div>
+        <footer>
+            <div id="userName">
+                <img className="profileCard" id='photoProfile' src={icon} alt='photoProfile'/>
+                <h5>Abhinandan Thour</h5>
+                <p>abhinandanthour2001@gmail.com</p>  
+            </div>
+            <div id="userSocial">
+                <h5>Contact me at:</h5>
+                <div className="social" id="gitHub">
+                    <img src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"/>
+                    <a href="#">GitHub</a>
+                </div>
+                <div className="social" id="LinkedIn">
+                    <img src="https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"/>
+                    <a href="#">LinkedIn</a>
+                </div>
+            </div>
+        </footer>
     );
 }
 
