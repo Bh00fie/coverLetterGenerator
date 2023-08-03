9eb7408ccedbbe0404c4c2e4f73a1c93148e19e4
Abhi <Bh00fie@gmail.com>
2023-08-03 20:51:21 +0100

    created a variable to create a paragraph (that will be sent to AI to create the cover letter) using user input values

---
+++ src/components/UserInput/API.js
@@ -0,0 +1,10 @@
+import React from "react";
+import UserInput from "./userInput";
+
+function ChatGPT() {
+
+    return (
+
+    );
+    
+}
--- src/components/UserInput/userInput.js
+++ src/components/UserInput/userInput.js
@@ -7,35 +7,32 @@ import Submit from "./submitButton";
 
 function UserInput() {
 
+    // Defining variables
     const [userInformation, setUserInformation] = useState({
         fullName: "",
         positionName: "",
         companyName: ""
     });
-
     const [JDInformation, setJDInformation] = useState({
         JDValue: ""
     });
-    
     const [CVInformation, setCVInformation] = useState({
         CVValue: ""
     });
 
-
+    // Handle form changes 
     const handleUserInfoChange = (fieldName, value) => {
         setUserInformation(prevInfo => ({
             ...prevInfo,
             [fieldName]: value
         }));
     };
-
     const handleJDChange = (value) => {
         setJDInformation(prevInfo => ({
             ...prevInfo,
             JDValue: value
         }));
     }
-
     const handleCVChange = (value) => {
         setCVInformation(prevInfo => ({
             ...prevInfo,
@@ -43,11 +40,27 @@ function UserInput() {
         }));
     }
 
+    // Show submitted info in the console
     const handleGenerateClick = () => {
         console.log("User Information:", userInformation);  
         console.log("CV Information:", CVInformation);
         console.log("JD Information:", JDInformation);
+
+            // chatGPT input
+        const userRequest = `Hi there, I am creating writing a cover letter could you help me with this? my name is ${userInformation.fullName},  I am applying for the position of a ${userInformation.positionName} at ${userInformation.companyName}, my CV is: 
+        ${CVInformation.CVValue}
+        
+        The job description is: 
+        ${JDInformation.JDValue}
+        
+        Could you write me a cover letter that integrate my information and the job the description to show why I want to work for the company and why I am a good fit?
+        
+        Make that 4-5 paragraphs long and max 350 words`
+
+        console.log(userRequest);
     };
+
+
 
     return (
         <form>
