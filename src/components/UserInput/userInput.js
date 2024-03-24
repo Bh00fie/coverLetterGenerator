00b8a45ea94b1468efd08ffdc7ad6dfbce7e834e
Bh00fie <Bh00fie@gmail.com>
2024-03-24 13:20:51 +0000

    Updated API key and increased number of tokens from 5k to 10k)

--- src/components/UserInput/userInput.js
+++ src/components/UserInput/userInput.js
@@ -23,7 +23,7 @@ async function generateChatResponse(userRequest, language) {
                 },
             ],
             temperature: 1,
-            max_tokens: 1000,
+            max_tokens: 10000,
             top_p: 1,
             frequency_penalty: 0,
             presence_penalty: 0,
