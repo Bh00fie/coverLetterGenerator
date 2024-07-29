3741966d8bb14a174ab2761e6fd3d05d11575755
Bh00fie <Bh00fie@gmail.com>
2024-07-30 00:45:44 +0100

    allowed the user to create an account and to login to a blank page, a log out needs to be added, thus a way to make sure the user is kept logging in the website and to add a username which will be used to say welcome username

--- frontend/src/components/createAccount/LoginAccount.js
+++ frontend/src/components/createAccount/LoginAccount.js
@@ -1,9 +1,34 @@
-import React from "react";
-import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./createAccount.css";
 import logo from "../images/logo.png";
 
 function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleLogin = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await fetch('http://127.0.0.1:3000/api/auth/login', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ email, password }),
+            });
+            const result = await response.json();
+            if (response.ok) {
+                setMessage('Login successful!');
+                localStorage.setItem('jwtToken', result.token);
+                window.location.href = '/home'; // Redirect to home page
+            } else {
+                setMessage(result.error || 'Login failed');
+            }
+        } catch (error) {
+            setMessage('An error occurred. Please try again.');
+        }
+    };
+
     return (
         <div>
             <Link to="/">
@@ -12,27 +37,36 @@ function Login() {
             <div id="loginInput">
                 <div id="loginForm">
                     <p id="fontLogin">Login</p>
-                    <input
-                        className='input'
-                        id='email'
-                        placeholder='Email'
-                        type='text'
-                        name='user_name'
-                        required
-                    />
-                    <input
-                        className='input'
-                        id='password'
-                        placeholder='Password'
-                        type='text'
-                        name='user_name'
-                        required
-                    />
-                    <button id='LoginButton' className="generalButton" type="button"> Login! </button>
+                    <form id="loginForm" onSubmit={handleLogin}>
+                        <input
+                            className="input"
+                            id="emailLogin"
+                            placeholder="Email"
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
+                        <input
+                            className="input"
+                            id="passwordLogin"
+                            placeholder="Password"
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                        <button id="LoginButton" className="generalButton" type="submit">
+                            Login!
+                        </button>
+                    </form>
                     <p id="orAccount">or</p>
                     <Link to="/registration">
-                        <button id='RegisterButtonChange' className="generalButton" type="button"> Create an account! </button>
+                        <button id="RegisterButtonChange" className="generalButton" type="button">
+                            Create an account!
+                        </button>
                     </Link>
+                    {message && <p id="responseMessageLogin">{message}</p>}
                 </div>
             </div>
         </div>
--- frontend/src/components/createAccount/RegistrationAccount.js
+++ frontend/src/components/createAccount/RegistrationAccount.js
@@ -1,9 +1,32 @@
-import React from "react"
-import { Link } from "react-router-dom"; // Import Link from react-router-dom
-import "./createAccount.css"
-import logo from "../images/logo.png"
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./createAccount.css";
+import logo from "../images/logo.png";
 
-function Registration(){
+function Registration() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleRegister = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await fetch('http://127.0.0.1:3000/api/auth/register', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ email, password }),
+            });
+            const result = await response.json();
+            if (response.ok) {
+                setMessage('Registration successful!');
+            } else {
+                setMessage(result.error || 'Registration failed');
+            }
+        } catch (error) {
+            setMessage('An error occurred. Please try again.');
+        }
+    };
+
     return (
         <div>
             <a href="/">
@@ -11,40 +34,41 @@ function Registration(){
             </a>
             <div id="registrationInput">
                 <div id="registrationForm">
-                <p id="fontRegistration">Register</p>
-                    <input
-                        className='input'
-                        id='userName'
-                        placeholder='Username'
-                        type='text'
-                        name='user_name'
-                        required
+                    <p id="fontRegistration">Register</p>
+                    <form id="registrationForm" onSubmit={handleRegister}>
+                        <input
+                            className="input"
+                            id="email"
+                            placeholder="Email"
+                            type="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
                         />
-                    <input
-                        className='input'
-                        id='email'
-                        placeholder='Email'
-                        type='text'
-                        name='user_name'
-                        required
+                        <input
+                            className="input"
+                            id="password"
+                            placeholder="Password"
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
                         />
-                    <input
-                        className='input'
-                        id='password'
-                        placeholder='Password'
-                        type='text'
-                        name='user_name'
-                        required
-                        />
-                    <button id='RegistrationButton' className="generalButton" type = "button"> Create an account </button>
+                        <button id="RegistrationButton" className="generalButton" type="submit">
+                            Create an account
+                        </button>
+                    </form>
                     <p id="orAccount">or</p>
                     <Link to="/login">
-                        <button id='LoginButtonChange' className="generalButton" type="button"> Login! </button>
+                        <button id="LoginButtonChange" className="generalButton" type="button">
+                            Login!
+                        </button>
                     </Link>
+                    {message && <p id="responseMessage">{message}</p>}
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default Registration
+export default Registration;
