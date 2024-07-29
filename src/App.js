6ccd57efdd78718fbb91e5840d16da1e3ee75d68
Bh00fie <Bh00fie@gmail.com>
2024-07-29 02:07:04 +0100

    added registration and login page, next step would consist in linking the backend

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import UserInput from './components/UserInput/userInput';
 import Footer from './components/footer';
 import InitialSection from './components/initialSection';
-import Registration from './components/UserInput/createAccount/RegistrationAccount';
-import Login from './components/UserInput/createAccount/LoginAccount';
+import Registration from './components/createAccount/RegistrationAccount';
+import Login from './components/createAccount/LoginAccount';
 import './App.css';
 
 function App() {
@@ -12,10 +12,11 @@ function App() {
         <Router>
             <Routes>
                 {/* Define routes for each page */}
-                <Route path="/" element={<><InitialSection /> <UserInput/> <Footer /></>} />
+                <Route path="/" element={<><InitialSection /> <UserInput/></>} />
                 <Route path="/registration" element={<Registration />} />
                 <Route path="/login" element={<Login />} />
             </Routes>
+            <Footer />
         </Router>
     );
 }
--- src/components/UserInput/createAccount/RegistrationAccount.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-function Registration(){
-    return (
-        <p>Registration Page</p>
-    )
-}
-
-export default Registration
